f56fdefd6b1b12d5105b2c7f55240ef720841c2d
sabur-pro <saburmallaev@gmail.com>
2025-11-08 11:23:19 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   console.log('Serving static files from:', uploadsPath);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://127.0.0.1:3001', 'http://localhost:3002', 'http://127.0.0.1:3002'],
+    origin: ['http://localhost:3001', 'http://127.0.0.1:3001', 'http://localhost:3002', 'http://127.0.0.1:3002', 'http://135.125.182.46:3001', 'http://135.125.182.46:3002'],
     credentials: true,
   });
 
